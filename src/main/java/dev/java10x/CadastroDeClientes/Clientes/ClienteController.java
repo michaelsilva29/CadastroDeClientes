494e87be94aef020b2c6274176fd697662cb2e13
Michael Silva <michaelcrf@gmail.com>
2026-05-02 16:31:40 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/java10x/CadastroDeClientes/Clientes/ClienteController.java
+++ src/main/java/dev/java10x/CadastroDeClientes/Clientes/ClienteController.java
@@ -1,8 +1,6 @@
 package dev.java10x.CadastroDeClientes.Clientes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping  // Anotação para colocar todas as rotas no mesmo lugar
@@ -11,6 +9,36 @@ public class ClienteController {
     @GetMapping("/boasvindas") // pegar informações
     public String boasvindas(){
         return "Essa é a minha primeira mensagem nessa rota";
+    }
+
+    // Add Cliente (CREATE)
+    @PostMapping("/criar")
+    public String criarCliente(){
+        return "Cliente criado";
+    }
+
+    // Mostrar todos dos Clientes (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosClientes(){
+        return "Mostrar todos os Clientes";
+    }
+
+    // Mostrar Clientes por ID (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsClientesPorId(){
+        return "Mostrar Clientes por ID";
+    }
+
+    // Alterar dados dos Clientes (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarClientesPorId(){
+        return "Alterar Cliente por ID";
+    }
+
+    // Deletar ClienteS (DETELE)
+    @DeleteMapping("/detetarID")
+    public String deletarClientesPorId(){
+        return "Ninja deletado por ID";
     }
 
 
